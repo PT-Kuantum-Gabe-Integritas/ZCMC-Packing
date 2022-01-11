4fb3e4ee5736f0825d152c3639bf53881794e1be
Yogy Pratama <yogypr03@gmail.com>
2022-01-11 09:03:58 +0700

    Done Fix the Bugs

---
+++ Kuantum/Kuantum/Class/User/UserData.vb
@@ -0,0 +1,8 @@
+﻿Public Class UserData
+
+    Public Type As String
+    Public Name As String
+    Public Pass As String
+    Public Authorized As Boolean
+    Public Logged As Boolean
+End Class
---
+++ Kuantum/Kuantum/Interface/IUserManagement.vb
@@ -0,0 +1,10 @@
+﻿Public Interface IUserManagement
+
+    Function GetPass(type As String, Optional ByRef pass As String = "") As Boolean
+    Function GetUserList() As List(Of String)
+    Function ValidateUser(user As UserData) As Boolean
+    Function AddUser() As Boolean
+    Sub ClearUser()
+    Sub Open()
+    Sub Close()
+End Interface
